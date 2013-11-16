73552e34913e1ba08cd1b1d4654349c9b520613d
Jiahao Chen <jiahao@mit.edu>
2013-11-16 18:05:27 -0500

    Cleanup packed rectangular matrix routines

--- base/linalg/rectfullpacked.jl
+++ base/linalg/rectfullpacked.jl
@@ -9,7 +9,7 @@ end
 function Ac_mul_A_RFP{T<:BlasFloat}(A::Matrix{T})
     n = size(A, 2)
     C = LAPACK.sfrk!('N', 'U', 'T', 1.0, A, 0.0, Array(T, div(n*(n+1),2)))
-    return SymmetricRFP(C, 'N', 'U')
+    SymmetricRFP(C, 'N', 'U')
 end
 
 type TriangularRFP{T<:BlasFloat} <: AbstractMatrix{T}
@@ -19,14 +19,7 @@ type TriangularRFP{T<:BlasFloat} <: AbstractMatrix{T}
 end
 TriangularRFP(A::Matrix) = TriangularRFP(trttf!('N', 'U', A)[1], 'N', 'U')
 
-function full(A::TriangularRFP)
-    B = LAPACK.tfttr!(A.transr, A.uplo, A.data)[1]
-    if A.uplo == 'U' 
-        return triu!(B)
-    else
-        return tril!(B)
-    end
-end
+full(A::TriangularRFP) = (A.uplo == 'U' ? triu! : tril!)(LAPACK.tfttr!(A.transr, A.uplo, A.data)[1])
 
 type CholeskyDenseRFP{T<:BlasFloat} <: Factorization{T}
     data::Vector{T}

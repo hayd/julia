123c4da6bf80f260ebd26c2bec91f7d05d2b7785
Andy Hayden <andyhayden1@gmail.com>
2015-03-25 23:56:23 -0700

    ahem

--- base/abstractarray.jl
+++ base/abstractarray.jl
@@ -378,7 +378,7 @@ full(x::AbstractArray) = x
 
 function map(T::Union(Type{Integer}, Type{Signed}, Type{Unsigned}), a::Array)
     Ta = (t = eltype(a); method_exists(one, (t,)) ? typeof(T(one(t))) : T)
-    map!(T, similar(a, Ta, a))
+    map!(T, similar(a, Ta), a)
 end
 
 ## range conversions ##
